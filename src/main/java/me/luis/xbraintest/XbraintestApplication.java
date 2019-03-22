7a2a93fe96f65fcaaa01e7bc6e582ec49884b200
Luis <luiisflorido@gmail.com>
2019-03-22 00:15:03 -0300

    Commit inicial

---
+++ src/main/java/me/luis/xbraintest/XbraintestApplication.java
@@ -0,0 +1,13 @@
+package me.luis.xbraintest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XbraintestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XbraintestApplication.class, args);
+	}
+
+}
